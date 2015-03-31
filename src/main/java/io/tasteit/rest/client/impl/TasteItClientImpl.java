10c8472b9dea021a1c06023a1452be32cdc9c56c
Zihang Yin <yinzh009100@gmail.com>
2015-03-30 23:58:31 -0700

    remove property file parser & pass base url and port as input

--- src/main/java/io/tasteit/rest/client/impl/TasteItClientImpl.java
+++ src/main/java/io/tasteit/rest/client/impl/TasteItClientImpl.java
@@ -1,6 +1,5 @@
 package io.tasteit.rest.client.impl;
 
-import io.tasteit.rest.client.PropertiesParser;
 import io.tasteit.rest.client.TasteItClient;
 import io.tasteit.rest.service.model.exception.TasteItClientException;
 import io.tasteit.rest.service.model.exception.TasteItServiceException;
@@ -12,9 +11,7 @@ import io.tasteit.rest.service.model.response.GetRestaurantMenuResponse;
 import io.tasteit.rest.service.model.response.GetRestaurantResponse;
 
 import java.io.IOException;
-import java.net.URI;
 
-import javax.annotation.Nonnull;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -23,63 +20,30 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.lang3.StringUtils;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.grizzly.connector.GrizzlyConnectorProvider;
 
 public class TasteItClientImpl implements TasteItClient {
-    private static final Logger LOG = LogManager.getLogger(TasteItClientImpl.class);
-
-    private static final String SERVER_PROPERTIES_FILE = "server.properties";
-    private static final String BASE_URL = "BASE_URL";
-    private static final String PORT = "PORT";
 
     private final Client client;
     private final WebTarget webTarget;
 
-    private static TasteItClientImpl instance;
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                if (instance != null) {
-                    try {
-                        instance.close();
-                        instance = null;
-                    } catch (Exception error) {
-                        LOG.error("Failed to close TasteItClient", error);
-                    }
-                }
-            }
-        }, "TasteItClient-ShutdownHook"));
-    }
-
-    public static synchronized TasteItClientImpl get() {
-        if (instance != null) {
-            return instance;
-        }
-
+    public TasteItClientImpl(String baseUrl, Integer port) {
         try {
-            PropertiesParser serverPropertiesParser = new PropertiesParser(SERVER_PROPERTIES_FILE);
-            String baseUrl = serverPropertiesParser.getProperty(BASE_URL);
-            String port = serverPropertiesParser.getProperty(PORT);
-
+            if (StringUtils.isBlank(baseUrl) || port == null) {
+                throw new IllegalArgumentException("baseUrl and port should not be null or empty");
+            }
             ClientConfig clientConfig = new ClientConfig().connectorProvider(new GrizzlyConnectorProvider());
             clientConfig.register(JSONObjectMapperImpl.class);
-            Client client = ClientBuilder.newBuilder().withConfig(clientConfig).build();
-            return instance = new TasteItClientImpl(client, UriBuilder.fromUri(baseUrl).port(Integer.parseInt(port)).build());
+            this.client = ClientBuilder.newBuilder().withConfig(clientConfig).build();
+            this.webTarget = client.target(UriBuilder.fromUri(baseUrl).port(port).build());
         } catch (Exception error) {
             throw new RuntimeException("Failed while attempting to initialize TasteItClient", error);
         }
     }
 
-    private TasteItClientImpl(@Nonnull Client client, @Nonnull URI uri) {
-        this.client = client;
-        this.webTarget = client.target(uri);
-    }
-
-    public void close() throws IOException {
+    public void shutdown() throws IOException {
         client.close();
     }
     
