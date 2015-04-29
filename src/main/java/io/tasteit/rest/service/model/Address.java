1ff44c48873bf151ee0964f70bbfbc1567cbff57
Hongke Qin <qinhk198687@gmail.com>
2015-04-28 23:52:14 -0700

    Add setter for Address

--- src/main/java/io/tasteit/rest/service/model/Address.java
+++ src/main/java/io/tasteit/rest/service/model/Address.java
@@ -8,97 +8,121 @@ public class Address {
     private String zipCode;
     private String state;
     private String country;
-    
+
     public String getLocalAddressLineOne() {
-        return localAddressLineOne;
+	return localAddressLineOne;
     }
-    
+
     public String getLocalAddressLineTwo() {
-        return localAddressLineTwo;
+	return localAddressLineTwo;
     }
-    
+
     public String getCity() {
-        return city;
+	return city;
     }
-    
+
     public String getZipCode() {
-        return zipCode;
+	return zipCode;
     }
-    
+
     public String getState() {
-        return state;
+	return state;
     }
-    
+
     public String getCountry() {
-        return country;
+	return country;
     }
-    
+
+    public void setLocalAddressLineOne(String localAddressLineOne) {
+	this.localAddressLineOne = localAddressLineOne;
+    }
+
+    public void setLocalAddressLineTwo(String localAddressLineTwo) {
+	this.localAddressLineTwo = localAddressLineTwo;
+    }
+
+    public void setCity(String city) {
+	this.city = city;
+    }
+
+    public void setZipCode(String zipCode) {
+	this.zipCode = zipCode;
+    }
+
+    public void setState(String state) {
+	this.state = state;
+    }
+
+    public void setCountry(String country) {
+	this.country = country;
+    }
+
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((city == null) ? 0 : city.hashCode());
-        result = prime * result + ((country == null) ? 0 : country.hashCode());
-        result = prime
-                * result
-                + ((localAddressLineOne == null) ? 0 : localAddressLineOne
-                        .hashCode());
-        result = prime
-                * result
-                + ((localAddressLineTwo == null) ? 0 : localAddressLineTwo
-                        .hashCode());
-        result = prime * result + ((state == null) ? 0 : state.hashCode());
-        result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
-        return result;
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((city == null) ? 0 : city.hashCode());
+	result = prime * result + ((country == null) ? 0 : country.hashCode());
+	result = prime
+		* result
+		+ ((localAddressLineOne == null) ? 0 : localAddressLineOne
+			.hashCode());
+	result = prime
+		* result
+		+ ((localAddressLineTwo == null) ? 0 : localAddressLineTwo
+			.hashCode());
+	result = prime * result + ((state == null) ? 0 : state.hashCode());
+	result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+	return result;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Address other = (Address) obj;
-        if (city == null) {
-            if (other.city != null)
-                return false;
-        } else if (!city.equals(other.city))
-            return false;
-        if (country == null) {
-            if (other.country != null)
-                return false;
-        } else if (!country.equals(other.country))
-            return false;
-        if (localAddressLineOne == null) {
-            if (other.localAddressLineOne != null)
-                return false;
-        } else if (!localAddressLineOne.equals(other.localAddressLineOne))
-            return false;
-        if (localAddressLineTwo == null) {
-            if (other.localAddressLineTwo != null)
-                return false;
-        } else if (!localAddressLineTwo.equals(other.localAddressLineTwo))
-            return false;
-        if (state == null) {
-            if (other.state != null)
-                return false;
-        } else if (!state.equals(other.state))
-            return false;
-        if (zipCode == null) {
-            if (other.zipCode != null)
-                return false;
-        } else if (!zipCode.equals(other.zipCode))
-            return false;
-        return true;
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Address other = (Address) obj;
+	if (city == null) {
+	    if (other.city != null)
+		return false;
+	} else if (!city.equals(other.city))
+	    return false;
+	if (country == null) {
+	    if (other.country != null)
+		return false;
+	} else if (!country.equals(other.country))
+	    return false;
+	if (localAddressLineOne == null) {
+	    if (other.localAddressLineOne != null)
+		return false;
+	} else if (!localAddressLineOne.equals(other.localAddressLineOne))
+	    return false;
+	if (localAddressLineTwo == null) {
+	    if (other.localAddressLineTwo != null)
+		return false;
+	} else if (!localAddressLineTwo.equals(other.localAddressLineTwo))
+	    return false;
+	if (state == null) {
+	    if (other.state != null)
+		return false;
+	} else if (!state.equals(other.state))
+	    return false;
+	if (zipCode == null) {
+	    if (other.zipCode != null)
+		return false;
+	} else if (!zipCode.equals(other.zipCode))
+	    return false;
+	return true;
     }
 
     @Override
     public String toString() {
-        return "Address [localAddressLineOne=" + localAddressLineOne
-                + ", localAddressLineTwo=" + localAddressLineTwo + ", city="
-                + city + ", zipCode=" + zipCode + ", state=" + state
-                + ", country=" + country + "]";
+	return "Address [localAddressLineOne=" + localAddressLineOne
+		+ ", localAddressLineTwo=" + localAddressLineTwo + ", city="
+		+ city + ", zipCode=" + zipCode + ", state=" + state
+		+ ", country=" + country + "]";
     }
 }
